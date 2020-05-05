ff7094d3f7acc3db24e24c9f34effa3709544b20
hipbackcircle <monsteradoll@ak.em-net.ne.jp>
2020-05-05 11:25:25 +0900

    Angularフロントエンド側作成

---
+++ src/app/products.ts
@@ -0,0 +1,39 @@
+export const products = [
+    {
+        coverImage: './assets/img/original.jpg',
+        name: 'Phone XL',
+        price: 799,
+        description: 'A large phone with one of the best screens',
+        heading1: 'sample text1',
+        heading2: 'sample text2',
+        heading3: 'sample text3',
+        headingtext1: 'sample text1',
+        headingtext2: 'sample text2',
+        headingtext3: 'sample text3'
+    },
+    {
+        coverImage: './assets/img/original.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        description: 'A great phone with one of the best cameras',
+        heading1: '',
+        heading2: '',
+        heading3: '',
+        headingtext1: '',
+        headingtext2: '',
+        headingtext3: ''
+    },
+    {
+        coverImage: './assets/img/original.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        description: '',
+        heading1: '',
+        heading2: '',
+        heading3: '',
+        headingtext1: '',
+        headingtext2: '',
+        headingtext3: ''
+    }
+
+];
